5f03897297fb0747135bfbf3e164c65c2109a69d
jack.mcloughlin <jackmaca@msn.com>
2023-01-12 16:29:09 +1030

    added basic parse and report

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,29 @@
 import fs from 'fs';
 
-console.log('🐉 Typescript base env running 🚀');
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
 
+// enum - enumeration
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
