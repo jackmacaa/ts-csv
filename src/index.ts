41a9fbb20d7b59c400d0c7d224d4ac6407663858
jack.mcloughlin <jackmaca@msn.com>
2023-01-18 13:53:16 +1030

    added HtmlReport

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import { CsvFileReader } from './CsvFileReader';
 import { ConsoleReport } from './reportTargets/ConsoleReports';
 import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
+import { HtmlReport } from './reportTargets/HtmlReport';
 
 const csvFileReader = new CsvFileReader('football.csv');
 
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport())
-summary.buildAndPrintReport(matchReader.matches)
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+summary.buildAndPrintReport(matchReader.matches);
